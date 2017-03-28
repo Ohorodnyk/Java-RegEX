8018688d53189f52c4cd4cb9cf05c351cb826efc
ohorodnyk <o.ohorodnyk@gmail.com>
2017-03-28 10:23:58 +0300

    add example

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+import java.util.regex.*;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Pattern p =Pattern.compile("[a-z]+");
+		
+		
+
+	}
+
+}
